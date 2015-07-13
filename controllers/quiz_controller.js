be7f8b3076c5878eb670a75467c45ee115e5acbd
Miguel Palomino <migueces@hotmail.com>
2015-07-13 18:04:03 +0200

    autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,20 +1,30 @@
 var models = require('../models/models.js')
-// GET /quizes/question
-exports.show = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
-	})
-};//GET /quizes/answer
-exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
-
-		} else {
-			res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
+// Autoload - factoriza el código si el path viene on quizID
+exports.load= function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz){
+			req.quiz = quiz;
+			next();
+			} else { next (new Error('No existe quizId= ' + quizId));}
 
 		}
-	})
+	).catch(function(error) { next(error);});
+};
+
+// GET /quizes/question
+exports.show = function(req, res){
+			res.render('quizes/show', {quiz: req.quiz});
+	};
+//GET /quizes/answer
+exports.answer = function(req, res) {
+	var resultado = 'Incorrecto'
+		if (req.query.respuesta === req.quiz.respuesta){
+			resultado = 'Correcto';
+
+		} 
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+
 };
 //GET quizes
 exports.index = function(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
