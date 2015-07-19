49699742930eeec836d73e2f73775217a6d08ca5
Miguel Palomino <migueces@hotmail.com>
2015-07-19 20:46:46 +0200

    Edita preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -68,3 +68,30 @@ exports.create = function(req,res) {
 		}
 	);
 };
+
+//GET /quizes/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+//PUT /quizes/:quizId
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err){
+			if (err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors });
+			} else {
+				req.quiz
+				.save( { fields: ["pregunta", "respuesta"] })
+				.then(function(){ res.redirect('/quizes');});
+
+			}
+		}
+		);
+
+};
