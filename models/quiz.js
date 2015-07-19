d8847a5710043baffe70fee9ccfba8f91f87b09b
Miguel Palomino <migueces@hotmail.com>
2015-07-19 18:40:06 +0200

    Validar DB

--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,14 @@
 //Definicion del modelo Quiz
 
 module.exports = function (sequelize, DataTypes){
-	return sequelize.define('Quiz', {pregunta: DataTypes.STRING,
-									respuesta: DataTypes.STRING,
-									});
+	return sequelize.define('Quiz', { 	pregunta:{ 
+											type: DataTypes.STRING,
+											validate: { notEmpty: {msg: "--> Falta pregunta"}}
+										},
+										respuesta:{ 
+											type: DataTypes.STRING,
+											validate: { notEmpty: {msg: "--> Falta respuesta"}}
+										}
+									}
+	);
 }
--- routes/author.js
+++ routes/author.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.render('author', {title: 'Creditos'});
+	res.render('author', {title: 'Creditos', errors: [] });
 });
 
 module.exports = router;
