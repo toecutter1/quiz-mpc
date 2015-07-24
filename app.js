cfa1be289b00e4a0c6ae0d59343a38f57f0cdf59
Miguel Palomino <migueces@hotmail.com>
2015-07-24 17:36:01 +0200

    Crear sesión

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser =        require('body-parser');
 var partials    =       require('express-partials');
 var methodOverride =    require('method-override');
 var routes =            require('./routes/index');
+var session =           require('express-session');
 
 var author =            require('./routes/author');
 
@@ -21,10 +22,24 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
-app.use(cookieParser());
+app.use(cookieParser('mpc'));
+app.use(session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
 app.use(methodOverride('_method'));
+
+//Helpers dinamicos
+
+app.use(function(req, res, next){
+    //guardar path de session para despues de login
+    if (!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;
+    }
+        //hacer visible session en las vistas
+        res.locals.session = req.session;
+        next();
+});
+
 
 app.use('/', routes);
 app.use('/author', author);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-var commentController = require('../controllers/comment_controller')
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,6 +11,11 @@ router.get('/', function(req, res) {
 });
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+
+router.get('/login',	sessionController.new);
+router.post('/login',	sessionController.create);
+router.get('/logout', sessionController.destroy);
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
